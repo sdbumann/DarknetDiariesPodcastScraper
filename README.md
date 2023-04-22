853acff764f00e73159bcc9770f7f344e2c57c7e
Samuel Bumann <bumann.samuel@gmail.com>
2023-04-22 18:54:13 +0200

    added more functionalities

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This is a Python script that scrapes all the episodes of the [Darknet Diaries](h
 * [Installation](#installation)
 * [Usage](#usage)
 * [Functionality](#functionality)
+* [Functions](#functions)
 * [Note](#note)
 * [License](#license)
 * [Contact](#contact)
@@ -22,18 +23,69 @@ This is a Python script that scrapes all the episodes of the [Darknet Diaries](h
 <!-- INSTALLATION -->
 ## Installation
 1. Clone this repository to your local machine using **'git clone https://github.com/sdbumann/DarknetDiariesPodcastScraper'**.
-2. Install the required packages by running **'pip install -r requirements.txt'**.
+2. Navigate into the directory: **'cd Darknet Diaries Podcast Scraper'**.
+3. Install the required packages by running **'pip install -r requirements.txt'**
+(or alternatively 'python setup.py' -> Note that for this the 'conda' package manger might be needed).
 
 <!-- USAGE -->
 ## Usage
 1. Navigate to the directory where you cloned this repository in your terminal.
-2. Run the script by executing **'python scraper.py'**.
+2. Run the script: **'python scraper.py [ARGUMENT]'**.
+3. Listen and enjoy podcast.
 
-The script will begin downloading the MP3 files for each episode to a folder called **'downloads'** in the current working directory. If the folder does not exist, the script will create one.
+The script takes an optional argument:
+
+latest: Downloads only the latest missing episode.
+* **'all'**: Downloads all episodes.
+* **'range min_episode max_episode'**: Downloads episodes within the specified range.
+* **'missing'**: Downloads only the missing episodes.
+* **'help'**: Displays usage instructions.
+If no argument is specified, the script displays the usage instructions.
+
+The script will begin downloading the MP3 files to a folder called **'downloads'** in the current working directory. If the folder does not exist, the script will create one.
+Note that it is also possible to change the folder and path.
 
 <!-- FUNCTIONALITY -->
 ## Functionality
-This script uses the **Selenium** and **Beautiful** Soup packages to scrape the Darknet Diaries website. These packages will be installed automatically when you run **'pip install -r requirements.txt'**. The script also uses the **'urllib.request.urlretrieve'** function to download the MP3 files.
+This script uses the **Selenium** and **Beautiful** Soup packages to scrape the Darknet Diaries website. These packages will be installed automatically when you run **'pip install -r requirements.txt'**.
+
+<!-- FUNCTIONS -->
+## Functions
+The following functions are defined in the script:
+
+**'scraper(base_url, downloads_folder, episode_numbers)'**'
+Scrapes the episodes with the specified episode numbers from the specified base URL and downloads them to the specified downloads folder.
+
+* **'base_url (str)'**: The base URL of the Darknet Diaries podcast.
+* **'downloads_folder (str)'**: The path of the downloads folder.
+* **'episode_numbers (list)'**: A list of episode numbers to scrape.
+
+**'get_latest_episode(base_url)'**'
+Gets the latest episode number from the specified base URL.
+
+* **'base_url (str)'**: The base URL of the Darknet Diaries podcast.
+
+**'get_episode_numbers(downloads_folder)'**'
+Gets a list of episode numbers that have already been downloaded from the specified downloads folder.
+
+* **'downloads_folder (str)'**: The path of the downloads folder.
+
+**'get_next_episode_number(downloads_folder)'**'
+Gets the next episode number to download from the specified downloads folder.
+
+* **'downloads_folder (str)'**': The path of the downloads folder.
+
+**'get_list_of_all_latest_missing_episodes(downloads_folder, max_eps_num)'**'
+Gets a list of the latest missing episodes from the specified downloads folder and the maximum episode number.
+
+* **'downloads_folder (str)'**: The path of the downloads folder.
+* **'max_eps_num (int)'**: The maximum episode number.
+
+**'get_list_of_all_missing_episodes(downloads_folder, all_episodes)'**'
+Gets a list of all missing episodes from the specified downloads folder and a list of all available episodes.
+
+* **'downloads_folder (str)'**: The path of the downloads folder.
+* **'all_episodes (list)'**: A list of all available episodes.
 
 <!-- NOTE -->
 ## Note
--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
 import time
 import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import urllib.request
 
-def scraper(base_url, downloads_folder, min_eps_num, max_eps_num):
+def scraper(base_url, downloads_folder, episode_to_download):
     """
     This function scrapes all the podcast episodes of the podcast Darknet Diaries from the website given by base_url.
     It downloads each episode's MP4 file and saves it in a folder specified by downloads_folder in the current working directory.
@@ -13,25 +14,17 @@ def scraper(base_url, downloads_folder, min_eps_num, max_eps_num):
     Args:
     base_url: A string representing the base URL of the Darknet Diaries website.
     downloads_folder: A string representing the path to the 'downloads' folder.
-    min_eps_num: An integer representing the minimum episode number to scrape.
-    max_eps_num: An integer representing the maximum episode number to scrape.
+    episode_to_download: A list of episode numbers to download
 
     Returns:
     -
     """
 
-    # specify the range of episode numbers you want to scrape
-    if min_eps_num == max_eps_num:
-        episode_range = [min_eps_num]
-    else:
-        episode_range = range(min_eps_num, max_eps_num)
-    print(episode_range)
-
     # set up the webdriver
     driver = webdriver.Chrome()
 
     # loop through each episode and scrape the download link
-    for episode in episode_range:
+    for episode in episode_to_download:
         print(f"Scraping episode {episode}...")
         # construct the URL for the current episode
         url = base_url + str(episode)
@@ -42,13 +35,6 @@ def scraper(base_url, downloads_folder, min_eps_num, max_eps_num):
 
         # switch to the iframe containing the episode page
         driver.switch_to.frame(driver.find_element(By.TAG_NAME, 'iframe')) 
-
-        # find the download button element by CSS selector
-        download_button = driver.find_element(By.CSS_SELECTOR, 'button.download-button')
-
-        # click the download button
-        download_button.click()
-        time.sleep(2)
 
         # find the mp3 link element and get its href attribute
         mp3_link = driver.find_element(By.CSS_SELECTOR, 'a.download-link-mp3')
@@ -63,6 +49,29 @@ def scraper(base_url, downloads_folder, min_eps_num, max_eps_num):
     # close the webdriver
     driver.quit()
 
+def get_episode_numbers(downloads_folder):
+    """
+    This function reads the names of all the files in the 'downloads' folder and returns a list of all episodes that exist. 
+    If there are no files in the folder, it returns [].
+
+    Args:
+    downloads_folder: A string representing the path to the 'downloads' folder.
+
+    Returns:
+    A sorted list of episode numbers that exist in the 'downloads' folder
+    """
+
+    episode_numbers = []
+    for filename in os.listdir(downloads_folder):
+        if filename.endswith(".mp3") and filename.startswith("Darknet Diaries S01E"):
+            episode_numbers.append(int(filename[len('Darknet Diaries S01E'):-len('.mp4')]))
+
+    if not episode_numbers:
+        return []
+
+    episode_numbers.sort()
+    return episode_numbers
+
 def get_next_episode_number(downloads_folder):
     """
     This function reads the names of all the files in the 'downloads' folder and returns the next highest episode
@@ -75,15 +84,10 @@ def get_next_episode_number(downloads_folder):
     An integer representing the next highest episode number that does not already exist in the 'downloads' folder.
     """
 
-    episode_numbers = []
-    for filename in os.listdir(downloads_folder):
-        if filename.endswith(".mp3") and filename.startswith("Darknet Diaries S01E"):
-            episode_numbers.append(int(filename[len('Darknet Diaries S01E'):-len('.mp4')]))
-
+    episode_numbers = get_episode_numbers(downloads_folder)
     if not episode_numbers:
         return 1
-
-    episode_numbers.sort()
+    
     return episode_numbers[-1] + 1
 
 def get_latest_episode(base_url):
@@ -112,6 +116,58 @@ def get_latest_episode(base_url):
                 latest_episode = episode_number
     return latest_episode
 
+def get_list_of_all_latest_missing_episodes(downloads_folder, max_eps_num):
+    """
+    This function retrieves the episode numbers of all the missing episodes between the last downloaded episode and the latest available episode. 
+    It first determines the last downloaded episode by checking the files in the "downloads" folder, 
+    then it determines the latest available episode by scraping the podcast website. 
+    Finally, it returns a list of all the missing episodes between these two numbers.
+
+    Args:
+    downloads_folder: A string representing the path to the 'downloads' folder.
+    max_eps_num: An integer representing the latest episode number available on the podcast website.
+
+    Returns:
+    episode_to_download: A list of all the missing episodes between the last downloaded episode and the latest available episode.
+    """
+
+    # get the minimum episode number to scrape by looking at the already downloaded episodes
+    min_eps_num = get_next_episode_number(downloads_folder)
+    print("Last downloaded episode: " + str(min_eps_num - 1).zfill(3) if str(min_eps_num - 1).zfill(3)!= '000' else "No episodes downloaded yet")
+    print("Next episode to download: " + str(min_eps_num).zfill(3))
+
+    # specify the range of episode numbers you want to scrape
+    if min_eps_num == max_eps_num:
+        episode_to_download = [min_eps_num]
+    elif min_eps_num < max_eps_num:
+        episode_to_download = list(range(min_eps_num, max_eps_num))
+    else:
+        episode_to_download = []
+
+    print("Episodes to download: " + str(episode_to_download))
+
+    return episode_to_download
+
+def get_list_of_all_missing_episodes(downloads_folder, all_episodes):
+    """
+    This function takes in two arguments: downloads_folder, which is the path to the folder containing already downloaded episodes, 
+    and all_episodes, which is a list of all available episode numbers. The function uses the get_episode_numbers function to 
+    retrieve the episode numbers of already downloaded episodes. It then generates a list of all missing episode numbers by 
+    comparing the list of all available episodes with the list of downloaded episodes. The missing episodes are returned as a list.
+
+    Args:
+    downloads_folder: A string representing the path to the 'downloads' folder.
+    all_episodes: A list of all available episode numbers.
+
+    Returns:
+    episode_to_download: A list of all missing episodes.
+    """
+
+    episode_numbers = get_episode_numbers(downloads_folder)
+    episode_to_download = [x for x in all_episodes if x not in episode_numbers]
+
+    return episode_to_download
+
 if __name__ == '__main__':
     # specify the base URL
     base_url = 'https://darknetdiaries.com/episode/'
@@ -121,18 +177,50 @@ if __name__ == '__main__':
     if not os.path.exists(downloads_folder):
         os.makedirs(downloads_folder)
 
-    # get the minimum episode number to scrape by looking at the already downloaded episodes
-    min_eps_num = get_next_episode_number(downloads_folder)
-    print("Last downloaded episode: " + str(min_eps_num - 1).zfill(3) if str(min_eps_num - 1).zfill(3)!= '000' else "No episodes downloaded yet")
-    print("Next episode to download: " + str(min_eps_num).zfill(3))
-
     # get latest episode number
     max_eps_num = get_latest_episode(base_url)
     print("Latest available episode: " + str(max_eps_num).zfill(3))
 
-    # scrape the episodes
-    if min_eps_num <= max_eps_num:
-        print(f'Scraping episodes {str(min_eps_num).zfill(3)} to {str(max_eps_num).zfill(3)}')
-        scraper(base_url, downloads_folder, min_eps_num, max_eps_num)
+    # list of all episodes
+    all_episodes = list(range(1,max_eps_num))
+
+    # check if the user has passed an argument to only download the latest missing episodes
+    episode_to_download=[]
+    if len(sys.argv) > 1 and sys.argv[1] == 'latest':
+        print("Getting list of latest missing episodes...")
+        episode_to_download = get_list_of_all_latest_missing_episodes(downloads_folder, max_eps_num)
+    elif len(sys.argv) > 1 and sys.argv[1] == 'all':
+        print("Getting list of all episodes...")
+        episode_to_download = all_episodes
+    elif len(sys.argv) > 1 and sys.argv[1] == 'missing':
+        print("Getting list of missing episodes...")
+        episode_to_download = get_list_of_all_missing_episodes(downloads_folder, all_episodes)
+    elif len(sys.argv) > 3 and sys.argv[1] == 'range':
+        print("Getting list of episodes in range...")
+        # check if the range is valid
+        if int(sys.argv[2]) > int(sys.argv[3]) or int(sys.argv[2]) < 1 or int(sys.argv[3]) < 1 or int(sys.argv[3]) > max_eps_num:
+            print("Invalid range. Exiting...")
+            sys.exit()
+        else:
+            episode_to_download = list(range(int(sys.argv[2]),int(sys.argv[3])))
+    elif len(sys.argv) > 1 and sys.argv[1] == 'help':
+        print('Please specify an argument: "latest", "all", "range min_episode max_episode", "missing"')
+        print('Example: python3 darknet_diaries.py latest')
+        print('Example: python3 darknet_diaries.py all')
+        print('Example: python3 darknet_diaries.py range 1 10')
+        print('Example: python3 darknet_diaries.py missing')
+        print('Exiting...')
     else:
-        print('No new episodes to scrape.')
+        print('Please specify an argument: "latest", "all", "range min_episode max_episode", "missing"')
+        print('Example: python3 darknet_diaries.py latest')
+        print('Example: python3 darknet_diaries.py all')
+        print('Example: python3 darknet_diaries.py range 1 10')
+        print('Example: python3 darknet_diaries.py missing')
+        print('Exiting...')
+
+    # scrape the episodes
+    if not episode_to_download:
+        print('Nothing to scrape.')
+    else:
+        print(f'Scraping episodes {episode_to_download}...')
+        scraper(base_url, downloads_folder, episode_to_download)
